15f87e12618d2cbe9609e810989759dacbd586c9
Sergiu VLAD D.A <sergiu@disruptive.academy>
2015-09-08 19:08:44 -0400

    Automatically detect protocol

--- server/config.js
+++ server/config.js
@@ -8,7 +8,7 @@ config.auto_start = true;
 
 config.app = {};
 config.app.debug = true; // set DEBUG=*,-not_this
-config.app.hostname = 'dev.ophmisu.com';
+config.app.hostname = 'ophmisu.com';
 config.app.httpPort = 2013;
 config.app.httpsPort = 2014;
 config.app.developer = ''; // developer's nickname
